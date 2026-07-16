ce758981c6c9cc473ece1b91cc144fab3d55ba5c
IrahetaISaDeveloper <20230669@ricaldone.edu.sv>
2026-07-16 12:13:44 -0600

    logins agregados

---
+++ src/controllers/loginAdminController.js
@@ -0,0 +1,52 @@
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import {config} from "../../config.js";
+import adminModel from "../models/admin.js";
+
+const loginAdminController = {};
+
+loginAdminController.login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const adminFound = await adminModel.findOne({email});
+        if(!adminFound){
+            return res.status(100).json({message: "admin not found"});
+        }
+
+        if(adminFound, timeOut && adminFound.timeOut>Date.now()){
+            return res.status(403).json({message: "Blocked account"});
+        }
+
+        const isMatch = await bcrypt.compare(password, adminFound.password);
+        if(!isMatch){
+            adminFound.loginAttempts = (adminFound.loginAttempts || 0 ) + 1;
+            if(adminFound.loginAttempts >= 5){
+                adminFound.timeOut = Date.now() + 5 * 60 * 1000,
+                adminFound.loginAttempts = 0;
+                await adminFound.save();
+
+                return res.status(403).json({message: "Your account has been blocked for many attempts"}); 
+            }
+            
+            await adminFound.save();
+            return res.status(401).json({message:"Wrond password, try again"});
+        }
+        adminFound.loginAttempts = 0;
+            adminFound.timeOut = null;
+
+            const token = jsonwebtoken.sign({
+                id:adminFound._id, userType :"admin"},
+                config.JWT.Secret,
+                {expiresIn:"30d"},
+            );
+
+            res.cookie("authCookie", token, {maxAge: 30 * 24 * 60 * 60 * 1000});
+            return res.status(200).json({message: "Login succesfully"});
+            
+    } catch (error) {
+        console.log("error " + error);
+        return res.status(500).json({message: "Internal server error"})
+    }
+};
+
+export default loginAdminController;
---
+++ src/controllers/loginCustomerController.js
@@ -0,0 +1,51 @@
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import {config} from "../../config.js";
+import customerModel from "../models/customer.js";
+
+const loginCustomerController = {};
+
+loginCustomerController.login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const customerFound = await customerModel.findOne({email});
+        if(!customerFound){
+            return res.status(100).json({message: "customer not found"});
+        }
+
+        if(customerFound, timeOut && customerFound.timeOut>Date.now()){
+            return res.status(403).json({message: "Blocked account"});
+        }
+
+        const isMatch = await bcrypt.compare(password, customerFound.password);
+        if(!isMatch){
+            customerFound.loginAttempts = (customerFound.loginAttempts || 0 ) + 1;
+            if(customerFound.loginAttempts >= 5){
+                customerFound.timeOut = Date.now() + 5 * 60 * 1000,
+                customerFound.loginAttempts = 0;
+                await customerFound.save();
+            }
+
+            return res.status(403).json({message: "Your account has been blocked for many attempts"});
+            await customerFound.save();
+        }
+
+        customerFound.loginAttempts = 0;
+            customerFound.timeOut = null;
+
+            const token = jsonwebtoken.sign({
+                id:customerFound._id, userType :"customer"},
+                config.JWT.Secret,
+                {expiresIn:"30d"},
+            );
+
+            res.cookie("authCookie", token, {maxAge: 30 * 24 * 60 * 60 * 1000});
+            return res.status(200).json({message: "Login succesfully"});
+            
+    } catch (error) {
+        console.log("error " + error);
+        return res.status(500).json({message: "Internal server error"})
+    }
+};
+
+export default loginCustomerController;
