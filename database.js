ad928047459afaa6747f8ccc9849402ad76b795f
IrahetaISaDeveloper <20230669@ricaldone.edu.sv>
2026-07-16 07:59:26 -0600

    Proyecto base

---
+++ database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import { config } from "./config.js";
+
+mongoose.connect(config.db.uri)
+
+const connection = mongoose.connection
+
+connection.once("open", () => {
+    console.log("DB is connected")
+});
+
+connection.on("disconnected", () =>{
+    console.log("DB is disconnected")
+});
+
+connection.on("error ", (error) =>{
+    console.log("error " + error)
+})
