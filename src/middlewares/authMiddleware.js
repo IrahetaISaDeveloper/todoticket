f9189538a1f17ece82a736a56e56cf7817e2c292
IrahetaISaDeveloper <20230669@ricaldone.edu.sv>
2026-07-16 08:24:05 -0600

    Authmiddleware agregado

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+import jsonwebtoken from "jsonwebtoken";
+import {config} from "../../config.js";
+
+export const validateAuthCookie = (allowedTypes = {}) =>{
+    return (req, res, next) =>{
+        try {
+            const {authCookie} = req.cookie;
+
+            if(!authCookie){
+                return res.status(403).json({message: "No cookie found, Authorize is required"})
+            }
+
+            const decoded = jsonwebtoken.verify(authCookie, config.JWT.Secret)
+
+            if(!allowedTypes.includes(decoded.userType)){
+                return res.status(401).json({message: "Acces denied"})
+            }
+            next()
+        } catch (error) {
+            console.log("error "+ error)
+            return res.status(500).json({message:"Internal server error"})
+        }
+    }
+}
